3a6a8fb5f622b2c142076a16a88968a3d623784f
hashbri <3108992364@qq.com>
2026-09-23 17:27:57 +0800

    统一为 number_guessing.py：保留带成绩记录与读取功能的版本

--- number_guessing.py
+++ number_guessing.py
@@ -15,28 +15,30 @@ if level==2:
 if level==3:
     max_num=200
     chance=5
-while True:
-    num = random.randint(1, max_num)
-    win=False
-    for i in range (chance):
-      a=int(input("请输入你猜测的数字："))
-      if a>num :
-          print("猜大了，还剩",chance-i-1,"次机会")
-      elif a<num:
-          print("猜小了,还剩",chance-i-1,"次机会")
-      elif a==num:
-          print("猜对了")
-          time.sleep(1)
-          print("恭喜你")
-          win=True
-          break
-    if win:
-        c=input("输入y再来一局：")
-        if c!="y":
+def number_game():
+    while True:
+      num = random.randint(1, max_num)
+      for i in range (chance):
+        a=int(input("请输入你猜测的数字："))
+        if a>num :
+            print("猜大了，还剩",chance-i-1,"次机会")
+        elif a<num:
+            print("猜小了,还剩",chance-i-1,"次机会")
+        elif a==num:
+            print("猜对了")
+            time.sleep(1)
+            print("恭喜你")
+            with open("scores.txt", "a", encoding="utf-8") as f:
+                f.write(f"猜了{i+1}次\n")
             break
-    else:
-        print("游戏失败,正确答案是:",num)
-        time.sleep(1)
-        b=input("输入y再来一局：")
-        if b!="y":
-            break
+      else:
+          print("游戏失败,正确答案是:", num)
+
+
+      b = input("输入y再来一局：")
+      if b != "y":
+        break
+    with open("scores.txt","r",encoding="utf-8")as f:
+        lines=f.readlines()
+    print(lines)
+number_game()
